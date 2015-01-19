02b736e84abeef7da49df9498c8e0507eb35848f
alonbasin <abtd2986@gmail.com>
2015-01-19 17:54:45 +0200

    CriminalIntent

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package com.bignerdranch.android.criminalintent.criminalintent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        //Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = Calendar.getInstance().getTime();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMM d, yyyy");
+        String myDate = sdf.format(mDate);
+        return myDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
